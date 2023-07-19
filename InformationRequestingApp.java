e6282ae24d79e9ab5ded17fb9b2bc4be643769fa
222774381 <139621723+222774381@users.noreply.github.com>
2023-07-19 21:23:18 +0200

    Add files via upload

---
+++ InformationRequestingApp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package informationrequestingapp;
+
+import za.ac.tut.ui.InformationRequestingForm;
+
+/**
+ *
+ * @author taarb
+ */
+public class InformationRequestingApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        InformationRequestingForm informationRequestingForm = new InformationRequestingForm();
+    }
+    
+}
